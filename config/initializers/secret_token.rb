6d82f1eea3b37199c6a2f0d07b6ec90a01d12a7d
Mike Waugh <mike.waugh@introhive.com>
2012-06-08 00:01:24 -0300

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beachpatrol::Application.config.secret_token = 'a21176bd81bd864f6be834b2104fa2be5740eb6bbb8e80fe2597270e301086e83c89fd3b7b238d760c23cbeb373754f03b5b743b8c7c99b8ce8375b7256ea32b'
